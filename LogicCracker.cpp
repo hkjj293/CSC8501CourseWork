32841d589e91e970a5d7d7e3b47ffd9caa6c0e3f
hkjj293 <chrishuihuihui@gmail.com>
2021-10-22 09:49:52 +0100

    change

--- LogicCracker.cpp
+++ LogicCracker.cpp
@@ -25,7 +25,10 @@ PWD LogicCracker::crack(std::string& pwd, std::chrono::nanoseconds& dur) {
 	cracked.time = std::chrono::nanoseconds::max();
 	time_point start_time = high_resolution_clock::now();
 	while ((nanoseconds)(high_resolution_clock::now() - start_time) < dur && labels[labels.size() - 1] <= pwd.size()) {
-		accum.assign(pwd.begin() + labels[labels.size()-2], pwd.begin() + labels[labels.size() - 1] -1);
+		for (int i = labels[labels.size() - 2]; i < labels[labels.size() - 1]; i++) {
+			accum += pwd[i];
+		}
+		//std::cout << accum << " " << pwd << std::endl;
 		std::vector<int> v = this->collatz.rev_collatz_map[this->collatz.collatz_string_map[accum]];
 		if (v.size() > 0) {
 			offset.push_back(this->collatz.collatz_string_map[accum]);
@@ -40,11 +43,14 @@ PWD LogicCracker::crack(std::string& pwd, std::chrono::nanoseconds& dur) {
 
 	}
 	labels.pop_back();
+	int i = 0;
 	for (auto iter = labels.begin(); iter < labels.end()-1; iter++) {
-		accum.assign(pwd.begin() + (*iter), pwd.begin() + *(iter));
-		test_pwd += (char)(this->collatz.collatz_string_map[accum] - offset[(*iter)]);
+		for (int i = labels[*iter]; i < labels[*(iter+1)]; i++) {
+			accum += pwd[i];
+		}
+		test_pwd += accum;
 	}
-	//std::cout << this->generate(test_pwd) << " - " << pwd << std::endl;
+	std::cout << this->generate(test_pwd) << " - " << pwd << std::endl;
 	std::cout << (this->generate(test_pwd) == pwd) << std::endl;
 	//std::cout << std::endl;
 	cracked.time = (nanoseconds)(high_resolution_clock::now() - start_time);
